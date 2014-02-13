96bf4d708a261304700dd6fa0159302191570a30
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-13 08:49:29 -0500

    adding rails empty project

---
+++ xims/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xims::Application.config.secret_key_base = 'd11e5eddf73efbdee1e470f79082808840f648873d09d170d5b9e75663bb3e2572b6ca5c6fb17bba3fc6c937a59946c70b178509d4b67fbfedbb3b1e8e305c7e'
