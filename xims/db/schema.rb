673ed0ff6611a5534410ae7fdbd3ebb70ece98d2
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-14 09:41:28 -0500

    upd schema after adding several models

---
+++ xims/db/schema.rb
@@ -0,0 +1,48 @@
+ # encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140213235125) do
+
+  create_table "employees", force: true do |t|
+    t.integer  "organization_id", null: false
+    t.integer  "individual_id",   null: false
+    t.integer  "workspace_id",    null: false
+    t.integer  "occupation_id",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "individuals", force: true do |t|
+    t.integer  "id_number",   null: false
+    t.string   "first_name",  null: false
+    t.string   "middle_name", null: false
+    t.string   "last_name",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "occupations", force: true do |t|
+    t.string "name", null: false
+  end
+
+  create_table "organizations", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "workspaces", force: true do |t|
+    t.string "name", null: false
+  end
+
+end
