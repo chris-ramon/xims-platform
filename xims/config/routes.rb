004184e9c03b5c595b5ea33d4b7ab13a3bff14f2
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-14 09:37:29 -0500

    adding /:organization/employees url.

--- xims/config/routes.rb
+++ xims/config/routes.rb
@@ -1,4 +1,6 @@
 Xims::Application.routes.draw do
+  get '/:organization_id/employees' => 'employees#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
