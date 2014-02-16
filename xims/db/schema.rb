70a56ec7b9a7f27da14c009801661aa5e5cbd250
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-15 23:59:38 -0500

    schema.rb

--- xims/db/schema.rb
+++ xims/db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213235125) do
+ActiveRecord::Schema.define(version: 20140214150459) do
 
   create_table "employees", force: true do |t|
     t.integer  "organization_id", null: false
@@ -40,6 +40,25 @@ ActiveRecord::Schema.define(version: 20140213235125) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "employee_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "workspaces", force: true do |t|
     t.string "name", null: false
