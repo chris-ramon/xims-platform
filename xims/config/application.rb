06084dbd98a9445f4461ad52c27e7e09dbc3b63d
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-15 23:47:56 -0500

    extending xims modules, so we can have custom exceptions.

--- xims/config/application.rb
+++ xims/config/application.rb
@@ -19,5 +19,9 @@ module Xims
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    require 'xims'
+
+    config.autoload_paths += Dir["#{Rails.root}/lib"]
   end
 end
