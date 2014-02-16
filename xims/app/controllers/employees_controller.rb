bf2ed430ae5d144ea8f824aee63338dc6a0a6d8f
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-15 23:54:39 -0500

    now employees_controller serves protected paginated list of employees.

--- xims/app/controllers/employees_controller.rb
+++ xims/app/controllers/employees_controller.rb
@@ -1,9 +1,22 @@
 class EmployeesController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
+    params.permit(:organization_id)
+    params.permit(:page)
+
     params.require(:organization_id)
 
-    employees = Employee.where(organization_id: params[:organization_id])
-    render json: employees
+    finder = Employee.where(organization_id: params[:organization_id])
+    employee = finder.first
+    guardian.ensure_can_see!(employee)
+
+    if finder.present?
+      finder = finder.page params[:page]
+      render json: {data: finder, metadata: {page: params[:page]}}
+    else
+      render json: {data: finder, metadata: {}}
+    end
   end
 
 end
--- xims/spec/controllers/employees_controller_spec.rb
+++ xims/spec/controllers/employees_controller_spec.rb
@@ -1,25 +1,69 @@
 require 'spec_helper'
 
 describe EmployeesController do
+
   describe 'show' do
-    it 'succeeds' do
-      3.times { create(:employee) }
-
-      xhr :get, :index
-      response.should be_success
-      parsed = JSON.parse(response.body)
-      parsed.length.should == 3
+    it "won't allow see the list of employees when user is not logged in" do
+      xhr :get, :index, organization_id: 1
+      response.should_not be_success
     end
-    it 'returns employees only for the given organization' do
-      abc_organization = create(:organization)
-      other_organization = create(:organization)
-      3.times { create(:employee, organization: abc_organization) }
-      3.times { create(:employee, organization: other_organization) }
+    describe 'when logged in' do
+      describe 'when user do not belongs to the given organization' do
+        before do
+          sign_in :user, create(:user)
+        end
+        it 'fails' do
+          xhr :get, :index, organization_id: 1
+          response.should_not be_success
+        end
+      end
+      describe 'when user belongs to the given organization' do
+        before do
+          @abc_organization = create(:organization)
+          @chris = create(:employee, organization: @abc_organization)
+          3.times { create(:employee, organization: @abc_organization) }
 
-      xhr :get, :index, organization_id: abc_organization.id
-      response.should be_success
-      parsed = JSON.parse(response.body)
-      parsed.length.should == 3
+          @user = create(:user, employee: @chris)
+          sign_in :user, @user
+        end
+        it 'succeeds' do
+          xhr :get, :index, organization_id: @abc_organization.id
+          response.should be_success
+          parsed = JSON.parse(response.body)
+          parsed['data'].length.should == 4
+        end
+        it 'returns employees only for the given organization' do
+          other_organization = create(:organization)
+          3.times { create(:employee, organization: other_organization) }
+
+          xhr :get, :index, organization_id: @abc_organization.id
+          response.should be_success
+          parsed = JSON.parse(response.body)
+          parsed['data'].length.should == 4
+        end
+      end
+      describe 'pagination' do
+        before do
+          @organization = create(:organization)
+          employee = create(:employee, organization: @organization)
+          user = create(:user, employee: employee)
+
+          @extra_total_per_page = 1
+          total_per_page = Kaminari.config.default_per_page
+          total_employees = total_per_page + @extra_total_per_page
+          total_employees.times { create(:employee, organization: @organization) }
+
+          sign_in user
+        end
+        it 'returns the given page' do
+          page = 2
+          xhr :get, :index, organization_id: @organization.id, page: page
+          response.should be_success
+          parsed = JSON(response.body)
+          parsed['metadata']['page'].should == page.to_s
+          parsed['data'].length.should == @extra_total_per_page + 1 # + 1 because we are creating one employee within before
+        end
+      end
     end
   end
 end
