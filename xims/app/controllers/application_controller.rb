56bd053d3f81d498d8a608025762d74dc374a8b9
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-15 23:48:54 -0500

    adding success_json, error_json, guardian methods to application controller.

--- xims/app/controllers/application_controller.rb
+++ xims/app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def success_json
+    {success: 'OK'}
+  end
+
+  def error_json
+    {failed: 'FAILED'}
+  end
+
+  def guardian
+    @guardian ||= Guardian.new(current_user)
+  end
+
+  rescue_from Xims::InvalidAccess do
+    render json: error_json, status: :unauthorized
+  end
+
 end
