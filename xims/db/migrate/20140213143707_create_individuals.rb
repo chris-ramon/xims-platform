5890c7637e77f7514875742e784f8d9b63534bc4
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-19 07:54:13 -0500

    adding columns to create individuals migration

--- xims/db/migrate/20140213143707_create_individuals.rb
+++ xims/db/migrate/20140213143707_create_individuals.rb
@@ -3,8 +3,9 @@ class CreateIndividuals < ActiveRecord::Migration
     create_table :individuals do |t|
       t.integer :id_number, null: false
       t.string :first_name, null: false
-      t.string :middle_name, null: false
+      t.string :middle_name
       t.string :last_name, null: false
+      t.string :second_last_name, null: false
       t.timestamps
     end
   end
