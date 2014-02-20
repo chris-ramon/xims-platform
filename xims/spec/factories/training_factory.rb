4d41482fb77ac89d10b8d4fcaca3aaf3490669b6
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-20 10:14:20 -0500

    adding training_employee, training_with_employees factory girl.

--- xims/spec/factories/training_factory.rb
+++ xims/spec/factories/training_factory.rb
@@ -7,5 +7,23 @@ FactoryGirl.define do
     sequence(:training_date) { |n| Time.now + n.weeks }
     training_hours { rand(1..50) }
     sequence(:topic) { |n| "#{n} topic" }
+
+    factory :training_with_employees do
+      ignore do
+        employees_count 5
+      end
+
+      after(:create) do |training, evaluator|
+        evaluator.employees_count.times {
+          employee = FactoryGirl.create(:employee, organization: training.organization)
+          create(:training_employee, training: training, employee: employee)
+        }
+      end
+    end
+  end
+
+  factory :training_employee do
+    training { FactoryGirl.create(:training) }
+    employee { FactoryGirl.create(:employee) }
   end
 end
