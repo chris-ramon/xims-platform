981d377408f889b5c527d6900c196e64b71c5597
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-23 12:27:10 -0500

    adding enforce_available_locales to false so we eliminate the warning about locales.

--- xims/config/application.rb
+++ xims/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Xims
   class Application < Rails::Application
+    # By now we don't care about locale validation
+    config.i18n.enforce_available_locales = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
