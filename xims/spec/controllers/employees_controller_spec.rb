94ba6a2b55c8eaf579a4f2c98b67ef1752a80ede
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-26 17:56:31 -0500

    refactoring employees ctrl spec.

--- xims/spec/controllers/employees_controller_spec.rb
+++ xims/spec/controllers/employees_controller_spec.rb
@@ -7,57 +7,47 @@ describe EmployeesController do
       xhr :get, :index, organization_id: 1
       response.should_not be_success
     end
-    describe 'when logged in' do
-      describe 'when user do not belongs to the given organization' do
-        before do
-          sign_in :user, create(:user)
-        end
+    context 'when logged in' do
+      let(:abc_organization) { create(:organization) }
+      let(:chris_as_employee) {create( :employee, organization: abc_organization) }
+      let(:chris_as_user) { create(:user, employee: chris_as_employee) }
+      let(:other_organization) { create(:organization) }
+      let(:add_employees_to_other_organization) { 3.times { create(:employee, organization: other_organization) }  }
+      before do
+        3.times { create(:employee, organization: abc_organization) }
+        sign_in chris_as_user
+      end
+      context 'when user do not belongs to the given organization' do
         it 'fails' do
-          xhr :get, :index, organization_id: 1
+          chris_as_employee.organization = other_organization
+          chris_as_employee.save
+          xhr :get, :index, organization_id: abc_organization.id
           response.should_not be_success
         end
       end
-      describe 'when user belongs to the given organization' do
-        before do
-          @abc_organization = create(:organization)
-          @chris = create(:employee, organization: @abc_organization)
-          3.times { create(:employee, organization: @abc_organization) }
-
-          @user = create(:user, employee: @chris)
-          sign_in :user, @user
-        end
+      context 'when user belongs to the given organization' do
         it 'succeeds' do
-          xhr :get, :index, organization_id: @abc_organization.id
+          xhr :get, :index, organization_id: abc_organization.id
           response.should be_success
         end
         it 'returns employees only for the given organization' do
-          other_organization = create(:organization)
-          3.times { create(:employee, organization: other_organization) }
-          xhr :get, :index, organization_id: @abc_organization.id
+          add_employees_to_other_organization
+          xhr :get, :index, organization_id: abc_organization.id
           response.should be_success
           parsed = JSON.parse(response.body)
           parsed['data'].length.should == 1
           parsed['metadata']['total_pages'].should == 4
         end
-      end
-      describe 'pagination' do
-        before do
-          @organization = create(:organization)
-          employee = create(:employee, organization: @organization)
-          user = create(:user, employee: employee)
-
-          @extra_total_per_page = 1
-          total_per_page = Kaminari.config.default_per_page
-          total_employees = total_per_page + @extra_total_per_page
-          total_employees.times { create(:employee, organization: @organization) }
-
-          sign_in user
-        end
-        it 'returns the given page' do
-          xhr :get, :index, organization_id: @organization.id, page: 2
+        it 'returns correct JSON format' do
+          xhr :get, :index, organization_id: abc_organization.id
           response.should be_success
-          parsed = JSON(response.body)
-          parsed['metadata']['page'].should == 2
+          parsed = JSON.parse(response.body)
+          parsed['data'][0].should have_key('id')
+          parsed['data'][0].should have_key('id_number')
+          parsed['data'][0].should have_key('first_name')
+          parsed['data'][0].should have_key('middle_name')
+          parsed['data'][0].should have_key('last_name')
+          parsed['data'][0].should have_key('second_last_name')
         end
       end
     end
@@ -95,4 +85,36 @@ describe EmployeesController do
       end
     end
   end
+
+  #describe 'alerts' do
+  #  context 'when user logged in' do
+  #    let(:abc_organization) { create(:organization) }
+  #    let(:chris_as_employee) { create(:employee, organization: abc_organization) }
+  #    let(:chris_as_user) { create(:user, employee: chris_as_employee) }
+  #    before do
+  #      sign_in chris_as_user
+  #    end
+  #    context 'user belongs to the organization' do
+  #      it 'succeeds' do
+  #        xhr :get, :alerts, organization_id: abc_organization
+  #        response.should be_success
+  #      end
+  #      it 'returns the correct data' do
+  #        xhr :get, :alerts, organization_id: abc_organization
+  #        parsed = JSON(response.body)
+  #        parsed['expired_risk_insurance']['total'].should == 1
+  #        parsed['expired_risk_insurance']['data'].should == 1
+  #        parsed['expired_medical_exam']['total'].should == 1
+  #        parsed['without_induction']['total'].should == 3
+  #      end
+  #    end
+  #    context 'user does not belong to the organization' do
+  #      other_organization = create(:organization)
+  #      chris_as_employee.organization = other_organization
+  #      chris_as_employee.save
+  #      xhr :get, :alerts, organization_id: other_organization.id
+  #      response.should_not be_success
+  #    end
+  #  end
+  #end
 end
