1567c9dc9386f7cba2e6c3400995dbf0499070a4
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-26 17:51:32 -0500

    now we respond with 401 when user is not authenticated instead of redirection.

---
+++ xims/lib/custom_failure.rb
@@ -0,0 +1,16 @@
+class CustomFailure < Devise::FailureApp
+  def respond
+    json_failure
+    #if request.format == :json
+    #  json_failure
+    #else
+    #  super
+    #end
+  end
+
+  def json_failure
+    self.status = 401
+    self.content_type = 'json'
+    self.response_body = "{failed: 'FAILED'}"
+  end
+end
