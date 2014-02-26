edb634e2493c587cee7e08d586200ee83ac33294
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-26 17:55:11 -0500

    adding custom SessionsController

---
+++ xims/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < Devise::SessionsController
+
+  def create
+    resource = warden.authenticate!(:scope => resource_name,
+                                    :recall => "#{controller_path}#failure")
+    sign_in(resource_name, resource)
+    render json: success_json
+  end
+
+  def destroy
+    sign_out(resource_name)
+    render nothing: true
+  end
+
+  def failure
+    warden.custom_failure!
+    render json: error_json, status: :unprocessable_entity
+  end
+
+  def show_current_user
+    warden.authenticate!(:scope => resource_name,
+                         :recall => "#{controller_path}#failure")
+    render json: current_user
+  end
+
+  #def new
+  #  render nothing: true
+  #end
+end
