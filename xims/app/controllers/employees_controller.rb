78b6f5d34c7a71932a6a0c91f78c482b18b4aa44
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-26 17:55:52 -0500

    now we return individual attributes for employees.

--- xims/app/controllers/employees_controller.rb
+++ xims/app/controllers/employees_controller.rb
@@ -4,10 +4,21 @@ class EmployeesController < ApplicationController
   def index
     params.permit(:page)
 
+    employee = Employee
+      .select('organization_id')
+      .where(organization_id: params[:organization_id])
+      .first
+
     finder = Employee
+      .joins(:individual)
+      .select('employees.id',  'individuals.id_number',
+              'individuals.first_name',
+              'individuals.middle_name',
+              'individuals.last_name',
+              'individuals.second_last_name')
       .where(organization_id: params[:organization_id])
       .page(params[:page])
-    employee = finder.first
+
     guardian.ensure_can_see!(employee)
     metadata = {page: params[:page].to_i,
                 total_pages: finder.total_pages.to_i}
