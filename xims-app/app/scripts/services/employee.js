4599147c1f579860614c8862ae23784e8434ce26
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-26 21:42:34 -0500

    upd EmployeeService.

--- xims-app/app/scripts/services/employee.js
+++ xims-app/app/scripts/services/employee.js
@@ -2,12 +2,14 @@
 
 
 angular.module('ximsApp')
-  .service('EmployeeService', ['$http', function($http) {
+  .service('EmployeeService', ['$http', 'UserService', function($http, UserService) {
     var self = this;
     self.getAll = function() {
+      var url = 'http://0.0.0.0:3000/:organization_id/employees';
+      url = url.replace(':organization_id', UserService.currentUser.organization.id);
       return $http({
         method: 'GET',
-        url: 'http://0.0.0.0:3000/1/employees'
+        url: url
       });
     }
   }]);
