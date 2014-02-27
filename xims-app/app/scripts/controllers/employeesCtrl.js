0f3d62d05825ed2073e02928ebc4fb117ae9d8fc
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-27 12:41:36 -0500

    setting meta data to scope.

--- xims-app/app/scripts/controllers/employeesCtrl.js
+++ xims-app/app/scripts/controllers/employeesCtrl.js
@@ -3,10 +3,16 @@
 angular.module('ximsApp')
   .controller('EmployeeListCtrl', function(ModuleService, EmployeeService, $scope) {
     ModuleService.name = ModuleService.EMPLOYEE;
+    $scope.employees = [];
+    $scope.meta = {};
     EmployeeService.getAll()
       .success(function(response) {
         $scope.employees = response.data;
+        $scope.meta['total_pages'] = response.meta.total_pages;
+        $scope.meta['current_page'] = response.meta.current_page;
       });
+    $scope.getTotalPages = function() { return new Array($scope.meta.total_pages); }
+    $scope.isActivePage = function(index) { return (index + 1) == $scope.meta.current_page; }
   })
   .controller('EmployeeCtrl', function(ModuleService, $routeParams, $scope) {
     ModuleService.name = ModuleService.EMPLOYEE;
