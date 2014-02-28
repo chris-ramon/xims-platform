5d07137dffa200206c7acee786f71be5fcb0b34f
Christian Ramon <rchristian.ramon@gmail.com>
2014-02-28 18:24:39 -0500

    refactoring EmployeeListCtrl

--- xims-app/app/scripts/controllers/employeesCtrl.js
+++ xims-app/app/scripts/controllers/employeesCtrl.js
@@ -2,24 +2,48 @@
 
 angular.module('ximsApp')
   .controller('EmployeeListCtrl',
-    ['$scope', 'ModuleService', 'EmployeeService', 'AppSettings',
-      function($scope, ModuleService, EmployeeService, AppSettings) {
+    ['$scope', '$filter', 'ModuleService', 'EmployeeService', 'AppSettings',
+      function($scope, $filter, ModuleService, EmployeeService, AppSettings) {
     ModuleService.name = ModuleService.EMPLOYEE;
     $scope.employees = [];
+    $scope.employeesCache = [];
     $scope.meta = {};
     $scope.totalItems = 0;
     $scope.currentPage = 0;
+    $scope.searchText = "";
+    $scope.searchingEmployees = true;
     $scope.maxSize = AppSettings.pagination.maxSize;
     $scope.itemsPerPage = AppSettings.pagination.itemsPerPage;
+
     $scope.setEmployees = function(page) {
       EmployeeService.getAll(page)
         .success(function(response) {
           $scope.employees = response.data;
+          $scope.employeesCache = response.data;
           $scope.totalItems = response.meta.total_items;
           $scope.currentPage = response.meta.current_page;
+          $scope.searchingEmployees = false;
         });
     }
-    $scope.setEmployees();
+    
+    $scope.search = function() {
+      if($scope.searchingEmployees) { return; }
+      $scope.searchingEmployees = true;
+    }
+
+    $scope.$watch("searchText", function(newVal) {
+      setEmployeesFiltered(newVal);
+    });
+
+    function setEmployeesFiltered(newVal) {
+      var result = $filter('filter')($scope.employeesCache, newVal);
+      if(result.length) { $scope.employees = result; }
+      else { $scope.employees = $scope.employeesCache; }
+    }
+
+    (function() {
+      $scope.setEmployees();
+    })();
   }])
   .controller('EmployeeCtrl', function(ModuleService, $routeParams, $scope) {
     ModuleService.name = ModuleService.EMPLOYEE;
