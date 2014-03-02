03dc1dfb127f4d1ddd2db5ee66f1c3b397b1383f
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-01 20:10:01 -0500

    adding search_data to Individual model

--- xims/app/models/individual.rb
+++ xims/app/models/individual.rb
@@ -1,3 +1,10 @@
 class Individual < ActiveRecord::Base
   searchkick
+  has_one :employee
+
+  def search_data
+    attributes.merge(
+        organization_id: employee.try(:organization_id)
+    )
+  end
 end
