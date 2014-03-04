3c47551f7934f2011134571938bed808ed007049
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-04 18:38:41 -0500

    adding various has_many dependencies and with_individual scope.

--- xims/app/models/employee.rb
+++ xims/app/models/employee.rb
@@ -3,4 +3,16 @@ class Employee < ActiveRecord::Base
   belongs_to :individual
   belongs_to :workspace
   belongs_to :occupation
+
+  has_many :risk_insurances
+  has_many :medical_exams
+  has_many :training_employees
+  has_many :trainings, through: :training_employees
+
+  scope :with_individual, -> {
+    joins(:individual)
+      .select('employees.id', 'individuals.id_number',
+              'individuals.first_name', 'individuals.middle_name',
+              'individuals.last_name', 'individuals.second_last_name')
+  }
 end
