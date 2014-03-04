9c7f9edf7b94f00ea5e838f9ea8114e95f3de0d1
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-04 18:42:08 -0500

    now Training uses Enum from lib for types

--- xims/app/models/training.rb
+++ xims/app/models/training.rb
@@ -1,8 +1,13 @@
 class Training < ActiveRecord::Base
-  TRAINING_TYPE = {induction: 0}
-
   belongs_to :organization
   belongs_to :responsible, class_name: 'Individual'
   belongs_to :trainer, class_name: 'Individual'
+
   has_many :training_employees
+  has_many :employees, through: :training_employees
+
+  def self.types
+    @types ||= Enum.new(:induction, :training,
+                        :coaching, :emergency_drill)
+  end
 end
