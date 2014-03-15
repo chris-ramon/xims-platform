2551847b49a1edcdee374bec4a7b96a3a4647e56
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-15 11:13:22 -0500

    adding custom exception

--- xims/lib/xims.rb
+++ xims/lib/xims.rb
@@ -1,4 +1,7 @@
 module Xims
   # When they don't have permission to do something
   class InvalidAccess < Exception; end
+
+  # When something they want is not found
+  class NotFound < Exception; end
 end
