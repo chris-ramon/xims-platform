42649875509b9e25850cfa3ac9c3402ac6feb06b
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-18 13:18:40 -0500

    adding various methods

--- xims/app/serializers/employee_serializer.rb
+++ xims/app/serializers/employee_serializer.rb
@@ -1,11 +1,25 @@
 class EmployeeSerializer < ApplicationSerializer
   attributes :id,
+             :id_number,
+             :age,
              :first_name,
              :middle_name,
              :last_name,
-             :second_last_name
-  has_one :organization, serializer: OrganizationSerializer, embed: :objects
+             :second_last_name,
+             :display_name,
+             :workspace,
+             :occupation,
+             :risk_insurance,
+             :medical_exam
+  has_one :organization, serializer: OrganizationSerializer,
+          embed: :objects
 
+  def id_number
+    object.individual.id_number
+  end
+  def age
+    object.individual.age
+  end
   def first_name
     object.individual.first_name
   end
@@ -17,5 +31,20 @@ class EmployeeSerializer < ApplicationSerializer
   end
   def second_last_name
     object.individual.try(:second_last_name)
+  end
+  def display_name
+    "#{first_name} #{middle_name} #{last_name} #{second_last_name}"
+  end
+  def workspace
+    object.workspace.name
+  end
+  def occupation
+    object.occupation.name
+  end
+  def risk_insurance
+    object.risk_insurance
+  end
+  def medical_exam
+    object.medical_exam
   end
 end
