26f32f7b5cc7eea104fe36d7e233185e03fa9aef
Christian Ramon <rchristian.ramon@gmail.com>
2014-03-31 14:42:20 -0500

    we are moving xims-app to its independant repo

--- xims-app/app/scripts/services/appSettings.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ximsApp')
-  .value('AppSettings', {
-    pagination: {maxSize: 3, itemsPerPage: 10},
-    employeeAlertsType: {riskInsuranceExpired: 1,
-      medicalExamExpired: 2, noInductionTraining: 3}
-  });
