ddbf188012598c7fa588d77036ae26400dfb483a
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-28 12:22:25 -0500

    removing unneeded zeros because of we were getting index out of range error

--- xims/spec/factories/individual_factory.rb
+++ xims/spec/factories/individual_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :individual do
-    sequence(:id_number) { |n| "#{n}0000000".to_i }
+    sequence(:id_number) { |n| "#{n}0".to_i }
     sequence(:first_name) { |n| "first_name #{n}" }
     sequence(:second_last_name) { |n| "second_last_name #{n}" }
     sequence(:last_name) { |n| "last_name #{n}" }
