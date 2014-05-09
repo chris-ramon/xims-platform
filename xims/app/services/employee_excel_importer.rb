74cc4071232c463260910a923cf3e3410c059154
Christian Ramon <rchristian.ramon@gmail.com>
2014-05-09 11:26:04 -0500

    adding EmployeeExcelImporter service.

---
+++ xims/app/services/employee_excel_importer.rb
@@ -0,0 +1,98 @@
+require 'roo'
+
+class EmployeeExcelImporter
+  attr_reader :employees
+
+  def initialize(file_path, current_page=1, default_per_page=nil)
+    @start_row = 2
+    @file_path = file_path
+    @default_per_page = default_per_page || Kaminari.config.default_per_page
+    if current_page.present?
+      @current_page = current_page.to_i
+    else
+      @current_page = 1
+    end
+    @employees = []
+    @total_items = nil
+  end
+  def import_data
+    s = Roo::Excelx.new(@file_path)
+    (@start_row..s.last_row).each do |line|
+      id_number = s.cell(line, 'A')
+      names = s.cell(line, 'B')
+      last_names = s.cell(line, 'C')
+      age = s.cell(line, 'D')
+      workspace = s.cell(line, 'E')
+      occupation = s.cell(line, 'F')
+      company_years_of_experience = s.cell(line, 'G')
+      gender = s.cell(line, 'H')
+      occupation_turn = s.cell(line, 'I')
+      type_of_contract = s.cell(line, 'J')
+      occupation_years_of_experience = s.cell(line, 'K')
+      company_name = s.cell(line, 'L')
+
+
+      names = names.split(' ')
+      if names.length > 1
+        first_name = names[0]
+        middle_name = names[1]
+      else
+        first_name = names[0]
+        middle_name = nil
+      end
+
+      last_names = last_names.split(' ')
+      if last_names.length > 1
+        last_name = last_names[0]
+        second_last_name = last_names[1]
+      else
+        last_name = last_names[0]
+        second_last_name = nil
+      end
+
+      @employees << Individual.create(id_number: id_number, first_name: first_name,
+                                      middle_name: middle_name,  last_name: last_name,
+                                      second_last_name: second_last_name, age: age)
+    end
+    @employees
+  end
+  def json_data
+    data = []
+    s = Roo::Excelx.new(@file_path)
+    @last_row = (s.last_row - @start_row) + 1
+    @total_items = s.last_row - 1
+
+    #pagination
+    start_row = (@default_per_page * (@current_page - 1)) + 2
+    remainder_rows = @last_row % @default_per_page
+    is_last_page = ((@last_row.to_f / @default_per_page).ceil == @current_page)
+    if remainder_rows > 0 && is_last_page
+      end_row = (start_row + remainder_rows) - 1
+    else
+      end_row = (@default_per_page * @current_page) + 1
+    end
+
+    (start_row..end_row).each do |line|
+      id_number = s.cell(line, 'A')
+      names = s.cell(line, 'B')
+      last_names = s.cell(line, 'C')
+      age = s.cell(line, 'D')
+      workspace = s.cell(line, 'E')
+      occupation = s.cell(line, 'F')
+      company_years_of_experience = s.cell(line, 'G')
+      gender = s.cell(line, 'H')
+      occupation_turn = s.cell(line, 'I')
+      type_of_contract = s.cell(line, 'J')
+      occupation_years_of_experience = s.cell(line, 'K')
+      company_name = s.cell(line, 'L')
+
+      data << {id_number: id_number, names: names,
+               last_names: last_names, age: age,
+               workspace: workspace, occupation: occupation,
+               company_years_of_experience: company_years_of_experience,
+               gender: gender, occupation_turn: occupation_turn, type_of_contract: type_of_contract,
+               occupation_years_of_experience: occupation_years_of_experience, company_name: company_name}
+    end
+    {data: data, current_page: @current_page, total_items: @total_items}
+  end
+end
---
+++ xims/spec/services/employee_excel_importer_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe EmployeeExcelImporter do
+  let(:excel_file) { "#{Rails.root}/spec/fixtures/employees_to_import.xlsx" }
+  describe 'get_data' do
+    it 'should set attributes correctly' do
+      employee_excel_importer = EmployeeExcelImporter.new(excel_file)
+      employee_excel_importer.import_data
+      employee_excel_importer.employees.length.should == 10
+    end
+  end
+  describe 'to_json' do
+    it 'succeeds' do
+      employee_excel_importer = EmployeeExcelImporter.new(excel_file)
+      result = employee_excel_importer.json_data
+      result[:data].length.should == 1
+    end
+
+    context 'pagination' do
+      it 'succeeds' do
+        employee_excel_importer = EmployeeExcelImporter.new(excel_file,
+                                                                                                    current_page=2,
+                                                                                                    default_per_page=5)
+        result = employee_excel_importer.json_data
+        result[:data].length.should == 5
+
+        employee_excel_importer = EmployeeExcelImporter.new(excel_file,
+                                                            current_page=2,
+                                                            default_per_page=7)
+        result = employee_excel_importer.json_data
+        result[:data].length.should == 3
+
+        employee_excel_importer = EmployeeExcelImporter.new(excel_file,
+                                                            current_page=2,
+                                                            default_per_page=9)
+        result = employee_excel_importer.json_data
+        result[:data].length.should == 1
+      end
+    end
+  end
+end
